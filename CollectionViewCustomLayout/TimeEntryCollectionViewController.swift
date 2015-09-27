a4655d3b00ef80ce5f71ec1e8ed8a5fbcaa922ff
Ryan Harvey <rharvey@intertech.com>
2015-09-27 15:55:23 -0500

    Complete custom layout.

--- CollectionViewCustomLayout/TimeEntryCollectionViewController.swift
+++ CollectionViewCustomLayout/TimeEntryCollectionViewController.swift
@@ -13,7 +13,6 @@ class TimeEntryCollectionViewController: UICollectionViewController {
         let layout = TimeEntryCollectionLayout()
         layout.days = sampleDataByDay
         collectionView?.collectionViewLayout = layout
-        collectionView?.scrollEnabled = false
     }
     override func viewWillAppear(animated: Bool) {
         collectionView!.reloadData()
@@ -42,8 +41,14 @@ class TimeEntryCollectionViewController: UICollectionViewController {
         let dateLabel = cell.viewWithTag(1) as! UILabel
         let hoursLabel = cell.viewWithTag(2) as! UILabel
         
-        dateLabel.text = day.date
+        dateLabel.text = day.date.stringByReplacingOccurrencesOfString(" ", withString: "\n").uppercaseString
         hoursLabel.text = String(totalHours)
+        
+        let hours = String(totalHours)
+        let bold = [NSFontAttributeName: UIFont.boldSystemFontOfSize(16)]
+        let text = NSMutableAttributedString(string: "\(hours)\nHOURS")
+        text.setAttributes(bold, range: NSMakeRange(0, hours.characters.count))
+        hoursLabel.attributedText = text
         
         return cell
     }
